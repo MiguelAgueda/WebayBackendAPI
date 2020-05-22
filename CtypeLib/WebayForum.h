6a7c5ece80d0f94c94b0f5a2481c2fd45609a23b
Velignis <61849244+Velignis@users.noreply.github.com>
2020-05-22 11:29:22 -0700

    Updated file with many changes

--- CtypeLib/WebayForum.h
+++ CtypeLib/WebayForum.h
@@ -9,6 +9,7 @@
 
 struct ForumPostNode;
 
+typedef std::pair<std::shared_ptr<ForumPostNode>, std::vector<std::shared_ptr<ForumPostNode>>*> replyLocationPair;
 //both typedef and using cause new to create an overload error upon constructing new objects
 
 // shared pointers are used instead of unique pointers to be able to return them withfindReply()
@@ -17,6 +18,8 @@ struct ForumPostNode
 	std::string postUser;
 	std::string postContent;
 	std::vector<std::shared_ptr<ForumPostNode>> replies;
+	std::string postDate;
+	std::string postTime;
 };
 
 struct ForumOP : public ForumPostNode
@@ -28,27 +31,50 @@ class WebayForumData
 {
 	// Perhaps change all functions that check if memory is full to from void to bool or int to return if it was successful
 	public:
-		void addOppost(const std::string&, const std::string&, const std::string&);
-		void addReplyPost(const int&, const int[], const std::string&, const std::string&);
-
+		// User name, title, content, date, time
+		void addOpPost(const std::string&, const std::string&, const std::string&, const std::string&, const std::string&);
+		// OpId, ReplyId, user name, content, date, time
+		void addReplyPost(const int&, std::vector<int>, const std::string&, const std::string&, const std::string&, const std::string&);
+		// OpId
 		void deleteOpPost(const int&);
-		void deleteReplyPost(const int&, const int[]);
+		// OpId, ReplyId
+		void deleteReplyPost(const int&, std::vector<int>);
 
+		//OpId, content
 		void modifyOpPost(const int&, const std::string&);
-		void modifyReplyPost(const int&, const int[], const std::string&);
+		//OpId, ReplyId, content
+		void modifyReplyPost(const int&, std::vector<int>, const std::string&);
 
-		void searchPostTitle(const std::string&);
-		std::string returnPostContent(const int&, const int[]);
+		std::string searchPostTitle(const std::string&);
+		std::string searchUserDirectReplies(const int&, const std::string&, const char&);
+		std::vector<int> returnMatchOpIds(const std::string&);
+		std::vector<int> returnMatchDirectReplyIds(const int&, const std::string&);
+		std::string returnOpPostList(const std::vector<int>&);
+		std::string returnFullOpList(const char&);
+		std::string returnPostData(const int&, const char&);
 
 		void reserveListSpace();
+		void cleanUp();
 		void destroyData();
 
-		std::shared_ptr<ForumPostNode> findReply(const int&, const int[]);
+		replyLocationPair findReply(const int&, std::vector<int>);
+		
+
 
 	private:
 		std::vector<std::unique_ptr<ForumOP>> forumOpList;
-		std::vector<int> deletedOp;
+		std::vector<int> deletedOpList;
 		bool full();
+
+		std::string recurReplies(const int&, std::shared_ptr<ForumPostNode>);
+
+		int matchCount(const int&, const std::string&);
+		std::vector<std::string> splitString(std::string);
+		void upperCase(std::string&);
+		bool validChar(const char&);
+		std::string cleanString(const std::string&);
+		bool isNumChar(const char& input);
+		static bool sortBySecond(const std::pair<int, int>&, const std::pair<int, int>&);
 };
 
 
